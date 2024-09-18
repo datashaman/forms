782c6fe77f36dd371a69292acd1c969a1f89f376
Marlin Forbes <marlinf@datashaman.com>
2024-09-18 10:22:13 +0200

    wip

--- components/CodePanel.vue
+++ components/CodePanel.vue
@@ -2,7 +2,7 @@
     <div
         class="flex flex-col flex-grow overflow-y-auto border-r border-base-200"
     >
-        <code v-highlight class="typescript">
+        <code class="typescript">
             <slot></slot>
         </code>
     </div>
